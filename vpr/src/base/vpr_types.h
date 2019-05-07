22851683c387702d61d40f119e1d4d2762d92906
Alessandro Comodi <acomodi@antmicro.com>
2019-05-07 17:29:52 +0200

    vpr options: added option to disable check_route
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- vpr/src/base/vpr_types.h
+++ vpr/src/base/vpr_types.h
@@ -878,6 +878,7 @@ struct t_router_opts {
     int max_convergence_count;
     float reconvergence_cpd_threshold;
     std::string first_iteration_timing_report_file;
+    bool disable_check_route;
 };
 
 struct t_analysis_opts {
